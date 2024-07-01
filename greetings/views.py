b93d4307e6d95fe1b5b0444923e225782b0b1d55
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2024-07-01 10:26:12 +0100

    Intial commit

---
+++ greetings/views.py
@@ -0,0 +1,17 @@
+from django.http import JsonResponse
+
+
+# Create your views here.
+
+def hello(request):
+    name = request.GET.get('visitor_name', 'Mark')
+    client_ip = request.META.get('REMOTE_ADDR', '127.0.0.1')
+    location = "New York"
+
+    data = {
+        'client_ip': client_ip,
+        'location': location,
+        'greeting': f'Hello, {name}!, the temperature is 11 degree Celcius ins {location} '
+    }
+
+    return JsonResponse(data)
